5f4b4e7d697b2a949cd47657ce7821c0b367e870
nwiechowski <89143006+nwiechowski@users.noreply.github.com>
2022-04-05 17:21:13 +0200

    Add files via upload

---
+++ Simulink/WindowControl_vera_rtw/rtmodel.h
@@ -0,0 +1,34 @@
+/*
+ *  rtmodel.h:
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "WindowControl".
+ *
+ * Model version              : 19.37
+ * Simulink Coder version : 9.4 (R2020b) 29-Jul-2020
+ * C source code generated on : Tue Feb 22 12:52:57 2022
+ *
+ * Target selection: vera_target.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtmodel_h_
+#define RTW_HEADER_rtmodel_h_
+
+/*
+ *  Includes the appropriate headers when we are using rtModel
+ */
+#include "WindowControl.h"
+#define GRTINTERFACE                   0
+
+/* Macros generated for backwards compatibility  */
+#ifndef rtmGetStopRequested
+#define rtmGetStopRequested(rtm)       ((void*) 0)
+#endif
+#endif                                 /* RTW_HEADER_rtmodel_h_ */
